dd8b40088f3836c3e8937216b2e072b572733112
Maria Ivanuschenko <ivamarnic@yandex.ru>
2022-05-24 13:15:09 +0300

    fix: add readstream according to check requirements

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
-});
+
+const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'));
+
+stream.pipe(process.stdout);
