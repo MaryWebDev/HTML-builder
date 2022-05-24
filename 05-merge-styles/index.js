2b733d91c0dc1a63ca519094645b2a653956c6ee
Maria Ivanuschenko <ivamarnic@yandex.ru>
2022-05-24 14:19:26 +0300

    fix: bugfix with styles updating in 05 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,7 +3,17 @@ const fs = require('fs');
 
 
 async function getData(dirPath = path.join(__dirname, 'styles')) {
-  await fs.promises.unlink(path.join(__dirname, 'project-dist/bundle.css'));
+  fs.readdir(path.join(__dirname, 'project-dist'), (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+      if (file.toString() === 'bundle.css') {
+        fs.unlink(path.join(__dirname, 'project-dist/bundle.css'), err => {
+          if (err) throw err;
+        });
+      }
+    }
+  });
+
   fs.readdir(dirPath, {withFileTypes: true}, (err, files) => {
     if (err) throw err;
     for (const file of files) {
